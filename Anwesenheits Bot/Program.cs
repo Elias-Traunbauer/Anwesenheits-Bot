b4d55685544fc735d179927fb8be7f0b26b53357
Elias Traunbauer <e.traunbauer@HTBLALeonding.onmicrosoft.com>
2020-04-01 14:08:24 +0200

    Token Update

--- Anwesenheits Bot/Program.cs
+++ Anwesenheits Bot/Program.cs
@@ -1,11 +1,24 @@
-﻿using System;
+﻿using System.IO;
+using System;
 
 namespace Anwesenheits_Bot
 {
     class Program
     {
-        static string token = "Njg5MDgzNjY1MjQxOTk3MzU4.XoMCXQ.IEoJa7GVxpYHD1w98f2gQyd_uC8";
+        static string token = "";
 
-        static void Main(string[] args) => new Client(token).InitializeAsync().GetAwaiter().GetResult();
+        static void Main(string[] args)
+        {
+            if (File.Exists(Environment.CurrentDirectory + @"\token.txt"))
+            {
+                token = File.ReadAllText(Environment.CurrentDirectory + @"\token.txt");
+            }
+            else
+            {
+                return;
+            }
+
+            new Client(token).InitializeAsync().GetAwaiter().GetResult();
+        }
     }
 }
